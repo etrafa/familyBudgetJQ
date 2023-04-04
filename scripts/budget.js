17bf6ac211153db4966acfb8337baae01990cb00
Etem Senel <etemsenel@Lauras-Air.lan>
2023-04-04 11:54:59 -0500

    other pages created.

---
+++ scripts/budget.js
@@ -0,0 +1,5 @@
+$(function () {
+  $(".sidebar-placeholder").load("../sidebar.html");
+  $(".navbar-top-placeholder").load("../navbarTop.html");
+  $(".navbar-bottom-placeholder").load("../navbarBottom.html");
+});
