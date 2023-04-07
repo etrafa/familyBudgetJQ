f97c1a489ababcbab1024b3b666ac1f82c9f931a
Etem Senel <etemsenel@Lauras-Air.lan>
2023-04-07 08:18:11 -0500

    modal placeholder added to another pages.

--- scripts/categories.js
+++ scripts/categories.js
@@ -2,4 +2,7 @@ $(function () {
   $(".sidebar-placeholder").load("../sidebar.html");
   $(".navbar-top-placeholder").load("../navbarTop.html");
   $(".navbar-bottom-placeholder").load("../navbarBottom.html");
+  $("#new-transaction-modal-placeholder").load(
+    "../modals/addNewTransaction.html"
+  );
 });
